3002993797f506f16c3202951061cda2d6ac49ff
vi5a7 <udamvisal18@kit.edu.kh>
2021-02-02 21:10:06 +0700

    update the menu-items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import {Route} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
+
+
 
 function App() {
   return (
     <div>
-      <HomePage/>
+        <Route exact path='/' component={HomePage} />
     </div>
   );
 }
